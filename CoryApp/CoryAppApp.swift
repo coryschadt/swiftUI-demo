3b5e51b9fd7c06187e1e2c6a4a34053484cae18d
Cory Schadt <cory@inputlogic.ca>
2021-09-08 15:30:52 -0700

    Initial Commit

---
+++ CoryApp/CoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoryAppApp.swift
+//  CoryApp
+//
+//  Created by Cory Schadt on 2021-09-08.
+//
+
+import SwiftUI
+
+@main
+struct CoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
