6cfe56f78591d48fc125d3a202fa4e5fe90d858c
Cory Schadt <cory@inputlogic.ca>
2021-09-09 14:34:27 -0700

    Build menu row and card

---
+++ CoryApp/MenuView.swift
@@ -0,0 +1,71 @@
+//
+//  MenuView.swift
+//  CoryApp
+//
+//  Created by Cory Schadt on 2021-09-09.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                Text("Cory - 28% complete")
+                    .font(.caption)
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(12)
+                
+                MenuRow(title: "Account", icon: "gear")
+                MenuRow(title: "Billing", icon: "creditcard")
+                MenuRow(title: "Sign-out", icon: "person.crop.circle")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(LinearGradient.init(gradient: Gradient(colors: [Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(#colorLiteral(red: 0.8661957383, green: 0.8939802051, blue: 0.9558447003, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+            .padding(.horizontal, 30)
+            .overlay(
+                Image("cory")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .offset(y: -150)
+            )
+        }
+        .padding(.bottom, 30)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
+
+struct MenuRow: View {
+    var title: String
+    var icon: String
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(systemName: icon)
+                .font(.system(size: 20, weight: .light))
+                .imageScale(.large)
+                .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.643707633, green: 0.7350915074, blue: 0.8382223248, alpha: 1)))
+            Text(title)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .frame(width: 120, alignment: .leading)
+        }
+    }
+}
