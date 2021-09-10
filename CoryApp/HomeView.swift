c9f8565a70949e608b4927fa5ff00303ae97f9db
Cory Schadt <cory@inputlogic.ca>
2021-09-10 11:52:53 -0700

    Build List view

--- CoryApp/HomeView.swift
+++ CoryApp/HomeView.swift
@@ -11,26 +11,49 @@ struct HomeView: View {
     
     @Binding var showProfile: Bool
     
+    @State var showUpdate = false
+    
     var body: some View {
         VStack {
-            HStack {
+            HStack(spacing: 10) {
                 Text("Watching")
                     .font(.system(size: 28, weight: .bold))
                 Spacer()
                 AvatarView(showProfile: $showProfile)
+                
+                Button(action: {self.showUpdate.toggle()}) {
+                    Image(systemName: "bell")
+                        .renderingMode(.original)
+                        .font(.system(size: 16, weight: .medium))
+                        .frame(width: 36, height: 36)
+                        .background(Color.white)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 1)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 10)
+                        
+                }
+                .sheet(isPresented: $showUpdate) {
+                    Text("Cool")
+                }
             }
             .padding(.horizontal)
             .padding(.top, 30)
             
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 30) {
-                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                        SectionView()
+                HStack(spacing: 20) {
+                    ForEach(sectionData) { item in
+                        GeometryReader { geometry in
+                            SectionView(section: item)
+                                .rotation3DEffect(Angle(degrees:
+                                                            Double(geometry.frame(in:
+                                                                                    .global).minX - 30) / -20
+                                ), axis: (x: 0, y: 10, z: 0))
+                        }
+                        .frame(width: 275, height: 275)
                     }
                 }
-                .padding(.horizontal)
-                .padding(.leading, 14)
-                .padding(.top, 30)
+                .padding(30)
+                .padding(.bottom, 30)
             }
             
             Spacer()
@@ -45,20 +68,23 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 struct SectionView: View {
+    var section: Section
+    
     var body: some View {
         VStack {
             HStack(alignment: .top) {
-                Text("Prototype designs in SwiftUI")
+                Text(section.title)
                     .font(.system(size: 28, weight: .bold))
                     .frame(width: 160, alignment: .leading)
                     .foregroundColor(.white)
                 Spacer()
-                Image("Logo1")
+                Image(section.logo)
             }
             
-            Text("18 Sections".uppercased())
+            Text(section.text.uppercased())
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-            Image("Card1")
+            
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -66,8 +92,26 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("card1"))
+        .background(section.color)
         .cornerRadius(30)
-        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
     }
 }
+
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+let sectionData = [
+    Section(title: "Prototype designs in SwiftUI", text: "18 sections",
+            logo: "Logo1", image: Image("Card3"), color: Color("card3")),
+    Section(title: "Cory's neat thing", text: "20 sections",
+            logo: "Logo2", image: Image("Card2"), color: Color("card2")),
+    Section(title: "Cool", text: "11 sections",
+            logo: "Logo3", image: Image("Card1"), color: Color("card1"))
+]
